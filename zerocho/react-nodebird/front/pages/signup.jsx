10a8f48d9202fbd7695e5f54a35609177247ea46
phw11 <hyunwoo4847@gmail.com>
2019-05-09 18:46:16 +0900

    next 이용한 sns 회원가입 컴포넌트 생성

---
+++ zerocho/react-nodebird/front/pages/signup.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
+import { Form, Input, Checkbox, Button } from "antd";
+
+const SignUp = () => {
+  // Custom Hooks
+  const useInput = (initValue = null) => {
+    const [value, setter] = useState(initValue);
+    const handler = e => {
+      const { value } = e.target;
+
+      setter(value);
+    };
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = useInput("");
+  const [nick, onChangeNick] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  //const [term, onChangeTerm] = useInput("");
+
+  // const [id, setId] = useState("");
+  //const [nick, setNick] = useState("");
+  //const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+
+    if (!term) {
+      return setTermError(true);
+    }
+
+    // eslint-disable-next-line no-console
+    console.log({
+      id,
+      nick,
+      password,
+      passwordCheck,
+      term
+    });
+  };
+
+  // const onChangeId = e => {
+  //   setId(e.target.value);
+  // };
+
+  // const onChangeNick = e => {
+  //   setNick(e.target.value);
+  // };
+
+  // const onChangePassword = e => {
+  //   setPassword(e.target.value);
+  // };
+
+  const onChangePasswordCheck = e => {
+    setPasswordError(e.target.value !== password);
+    setPasswordCheck(e.target.value);
+  };
+
+  const onChangeTerm = e => {
+    setTerm(e.target.checked);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Node Bird</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.js" />
+      </Head>
+      <AppLayout>
+        <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" required value={id} onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nick">닉네임</label>
+            <br />
+            <Input
+              name="user-nick"
+              required
+              value={nick}
+              onChange={onChangeNick}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input
+              name="user-password"
+              required
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호 체크</label>
+            <br />
+            <Input
+              name="user-password-check"
+              required
+              type="password"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+            {passwordError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              제로초 말을 잘 들을 것을 동의합니다.
+            </Checkbox>
+            {termError && (
+              <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+            )}
+          </div>
+          <div>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
+      </AppLayout>
+    </>
+  );
+};
+
+export default SignUp;
